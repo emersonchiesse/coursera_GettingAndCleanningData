a0352e7034796ba5eccc66b98eeb4ffce5b7f7ad
Emerson Chiesse <emersonchiesse@gmail.com>
2014-10-26 21:22:06 -0200

    uncomment local changes

--- run_analysis.R
+++ run_analysis.R
@@ -1,11 +1,11 @@
 
+# prepare files
+u="https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+download.file(u,destfile = "data.zip", method="wget")
 
-#u="https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
-#download.file(u,destfile = "d.zip", method="wget")
-
-#print("unzipping data... please, wait")
-#unzip("d.zip")
-#setwd("UCI HAR Dataset")
+print("unzipping data... please, wait")
+unzip("data.zip")
+setwd("UCI HAR Dataset")
 
 # setting variables
 fileTrain="train/X_train.txt"
@@ -17,4 +17,8 @@ train=read.table(fileTrain, header=F)
 print ("reading test file...")
 test=read.table(fileTest, header=F)
 
-#data=merge(train, test)
+print ("merging data...")
+result=merge(train, test)
+
+print ("writing to file result.txt...")
+write.table(result, "result.txt", row.name=F)
